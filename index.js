56f34ab90637ec6590eea2c36cb72541ecc8b063
Uparelabrayan <bryansosa2001@hotmail.com>
2022-11-11 20:52:23 -0500

    Proyecto terminado

---
+++ index.js
@@ -0,0 +1,17 @@
+function moverPosicionRamdon(elm){
+    elm.style.position = 'absolute';
+    elm.style.top = Math.random()*(window.innerHeight - elm.offsetHeight) + 'px';
+    elm.style.left = Math.random()*(window.innerWidth - elm.offsetWidth) + 'px';
+}
+let btnSi = document.getElementById("btn_si");
+let btnNo = document.getElementById("btn_no");
+let divModoSexo = document.getElementsByClassName("modo_sexo")[0];
+
+btnNo. addEventListener("mouseenter", function(e){moverPosicionRamdon(e.target) });
+btnSi.addEventListener('click', function(e){
+    alert('Sabia que dirias que SI, TE AMO ❤');
+    divModoSexo.style.display= 'inline';
+    const cancion = new Audio('img_modo_hot.mp3');
+    cancion.play();
+    
+})
